3d236b8f4b1f8248672ab4573391a2fb526399e8
pruthvik49 <pruthvirajbhoir070@gmail.com>
2025-08-15 16:38:51 +0530

    java script variables

---
+++ javascript_/varible.js
@@ -0,0 +1,33 @@
+let a = 12;
+var b = 35;
+console.log(a + b)
+// var 44k = "koman";not allowed
+
+console.log(typeof a, typeof b)
+const a1 = 23
+a1 + 5; //not allowed beacuse a1 is constandt
+{
+    let a = 55
+    console.log(a) // let is blcok element only work in {}block
+}
+console.log(a) // var is globly scop 
+let x = 63;
+let y = 22;
+let z = 3.3;
+const p = true;
+let q = undefined;
+let r = null;
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    "name": "pruthvik",
+    "job code": 5600,
+    "is_smart": true
+}
+console.log(o)
+o.salary = "100cr"
+console.log(o)
+
+
+
