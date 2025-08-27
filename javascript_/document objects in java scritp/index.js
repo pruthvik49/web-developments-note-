9e400c8f46575e8326c74b8eb90b23ace2f13e6e
pruthvik49 <pruthvirajbhoir070@gmail.com>
2025-08-27 18:28:33 +0530

    color changing with math

--- javascript_/document objects in java scritp/index.js
+++ javascript_/document objects in java scritp/index.js
@@ -6,6 +6,18 @@
 // // // box[2].style.backgroundColor = "red"
 // // document.getElementById("reb").style.backgroundColor = "red"
 // document.querySelector(".box").style.backgroundColor = "green" is for only one or fist .box
-document.querySelectorAll(".box").forEach(e => {
-    e.style.backgroundColor = "green";
-});
+// 
+let boxes = document.querySelector(".contnr").children
+console.log(boxes)
+function getRandomColor(){
+    let val1 = Math.ceil(0 + Math.random()* 255);
+    let val2 = Math.ceil(0 + Math.random()* 255);
+    let val3 = Math.ceil(0 + Math.random()* 255);
+    return `rgb(${val1}, ${val2}, ${val3})`
+
+}
+Array.from(boxes).forEach(e=>{
+    e.style.backgroundColor = getRandomColor()
+    e.style.Color = getRandomColor()
+})
+
